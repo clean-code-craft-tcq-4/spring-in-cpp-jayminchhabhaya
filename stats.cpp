f42c98dc5c61e2617b249af3059c4aeeea8c87ab
jayminchhabhaya <33538525+jayminchhabhaya@users.noreply.github.com>
2022-05-09 20:46:24 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,15 @@
 #include "stats.h"
-
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+template <typename T>
+Stats Statistics::ComputeStatistics(const std::vector<T>& p_vector) {
     //Implement statistics here
+    Stats Object_stats;
+    Object_stats.max = *max_element(p_vector.begin(), p_vector.end();
+    Object_stats.min = *min_element(p_vector.begin(), p_vector.end());
+                 
+    T avrg;
+    for(std::size_t i = 0; i < p_vector.size(); i++)
+       avrg += p_vector.at(i);      
+   
+   Object_stats.average = avrg
+   return Object_stats;              
 }
