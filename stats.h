1f5ba0ce1d4e49e53cbfd81c57eaf08dbff7a121
jayminchhabhaya <33538525+jayminchhabhaya@users.noreply.github.com>
2022-05-09 20:42:11 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,14 @@
 #include <vector>
-
+template <typename T>
+class Stats
+{
+public:
+    T average{};
+    T max{};
+    T min{};
+    ~stats(){}
+};
+template <typename T>
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<T>& p_vector);
 }
