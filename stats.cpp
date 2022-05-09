495e127d685f6e1e24da947b33ff8859465d34a9
jayminchhabhaya <33538525+jayminchhabhaya@users.noreply.github.com>
2022-05-09 20:51:39 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,13 +2,13 @@
 Stats Statistics::ComputeStatistics(const std::vector<double>& p_vector) {
     //Implement statistics here
     Stats Object_stats;
-    Object_stats.max = *max_element(p_vector.begin(), p_vector.end();
+    Object_stats.max = *max_element(p_vector.begin(), p_vector.end());
     Object_stats.min = *min_element(p_vector.begin(), p_vector.end());
                  
     double avrg;
     for(std::size_t i = 0; i < p_vector.size(); i++)
        avrg += p_vector.at(i);      
    
-   Object_stats.average = avrg
+   Object_stats.average = avrg;
    return Object_stats;              
 }
