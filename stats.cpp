4a6956718c6536c78fd6833e56290760eee820b2
jayminchhabhaya <33538525+jayminchhabhaya@users.noreply.github.com>
2022-05-10 08:18:47 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,13 +5,16 @@ using namespace std;
 Stats Statistics::ComputeStatistics(const std::vector<float>& p_vector) {
 	//Implement statistics here
 	Stats Object_stats;
-	Object_stats.max = *max_element(p_vector.begin(), p_vector.end());
-	Object_stats.min = *min_element(p_vector.begin(), p_vector.end());
+	if (p_vector.size() > 0)
+	{
+		Object_stats.max = *max_element(p_vector.begin(), p_vector.end());
+		Object_stats.min = *min_element(p_vector.begin(), p_vector.end());
 
-	float avrg;
-	for (size_t i = 0; i < p_vector.size(); i++)
-		avrg += p_vector.at(i);
+		float avrg;
+		for (size_t i = 0; i < p_vector.size(); i++)
+			avrg += p_vector.at(i);
 
-	Object_stats.average = avrg;
+		Object_stats.average = avrg;
+	}
 	return Object_stats;
 }
